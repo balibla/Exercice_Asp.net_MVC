d922eb82cb3708ecfdaaf4b822539291f6160318
F.balibla <44398703+balibla@users.noreply.github.com>
2020-01-10 12:12:12 +0100

    first commit

---
+++ Controllers/FormationController.cs
@@ -0,0 +1,33 @@
+﻿using ExerciceAspnetMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExerciceAspnetMvc.Controllers
+{
+    public class FormationController : Controller
+    {
+        // GET: Formation
+        public ActionResult DetailFormation(string categorie,string nomSeo)
+        {
+            var vm = new FormationViewModel();
+            vm.Categorie = categorie;
+            vm.NomSeo = nomSeo;
+            return View(vm);
+        }
+        public ActionResult FormationParCatégorie(string categorie)
+        {
+
+            return View();
+        }
+
+        public ActionResult ToutesLesFormations()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Models/FormationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExerciceAspnetMvc.Models
+{
+    public class FormationViewModel
+    {
+        public string Categorie { get;  set; }
+        public string NomSeo { get;  set; }
+    }
+}
